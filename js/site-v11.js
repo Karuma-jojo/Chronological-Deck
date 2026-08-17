08fce14b4816643da7c6d5d634e6b804348de526
Karuma-jojo <kareemgambit@gmail.com>
2026-08-17 10:28:32 +0530

    Bump bootstrap for T23

--- js/site-v11.js
+++ js/site-v11.js
@@ -3,6 +3,7 @@
 // Integrity markers retained for the existing Pages wiring checks:
 // import "./data/law-expansion.js";
 // await import("./data/t22-quant-research.js")
+// await import("./data/t23-universal-scientist.js")
 // await import("./app.js")
 // await import("./vault.js")
-await import("./site-v12.js?v=1.2.2");
+await import("./site-v12.js?v=1.4.0");
