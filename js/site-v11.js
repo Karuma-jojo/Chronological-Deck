a7894a23d43e832e1abfa8f293cdab0ea6bb0e11
Karuma-jojo <kareemgambit@gmail.com>
2026-08-27 23:05:56 +0530

    Preserve bootstrap integrity marker

--- js/site-v11.js
+++ js/site-v11.js
@@ -8,4 +8,5 @@
 // await import("./app.js")
 // await import("./t22-atomic-ui.js")
 // await import("./vault.js")
+// Previous cache-key marker: site-v12.js?v=1.7.0
 await import("./site-v12.js?v=1.7.1");
