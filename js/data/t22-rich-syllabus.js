1bdd0bc7b8b831a501c5f66edaf773a765fb0290
Karuma-jojo <kareemgambit@gmail.com>
2026-08-28 05:42:30 +0530

    Wire T22 Module 36 into rich registry

--- js/data/t22-rich-syllabus.js
+++ js/data/t22-rich-syllabus.js
@@ -32,6 +32,7 @@ import { buildT22RichModule32 } from "./t22-rich-module-32.js";
 import { buildT22RichModule33 } from "./t22-rich-module-33.js";
 import { buildT22RichModule34 } from "./t22-rich-module-34.js";
 import { buildT22RichModule35 } from "./t22-rich-module-35.js";
+import { buildT22RichModule36 } from "./t22-rich-module-36.js";
 
 export const T22_RICH_SYLLABUS_VERSION = "3.0";
 
@@ -293,6 +294,7 @@ const MODULE_32 = buildT22RichModule32(T22_RICH_SYLLABUS_VERSION);
 const MODULE_33 = buildT22RichModule33(T22_RICH_SYLLABUS_VERSION);
 const MODULE_34 = buildT22RichModule34(T22_RICH_SYLLABUS_VERSION);
 const MODULE_35 = buildT22RichModule35(T22_RICH_SYLLABUS_VERSION);
+const MODULE_36 = buildT22RichModule36(T22_RICH_SYLLABUS_VERSION);
 
 export const T22_RICH_MODULES = Object.freeze({
   ARC053: MODULE_1,
@@ -330,6 +332,7 @@ export const T22_RICH_MODULES = Object.freeze({
   ARC509: MODULE_33,
   ARC513: MODULE_34,
   ARC541: MODULE_35,
+  ARC542: MODULE_36,
 });
 
 export function getT22RichModule(moduleId) {
