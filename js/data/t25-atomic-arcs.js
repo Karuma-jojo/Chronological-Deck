991236c7b8ac926da4ee1e7c20f934a92273e684
Karuma-jojo <kareemgambit@gmail.com>
2026-09-14 16:55:41 +0530

    Register audited T25 v2 cards 123-126

--- js/data/t25-atomic-arcs.js
+++ js/data/t25-atomic-arcs.js
@@ -45,6 +45,7 @@ import { T25_ATOMIC_V4_109_112 } from "./t25-atomic-v4-109-112.js";
 import { T25_ATOMIC_V4_113_116 } from "./t25-atomic-v4-113-116.js";
 import { T25_ATOMIC_V4_117_118 } from "./t25-atomic-v4-117-118.js";
 import { T25_ATOMIC_V4_119_122 } from "./t25-atomic-v4-119-122.js";
+import { T25_ATOMIC_V4_123_126 } from "./t25-atomic-v4-123-126.js";
 
 export const T25_ATOMIC_AUDIT_VERSION = T25_MSTAT_AUDIT_VERSION;
 export { T25_MSTAT_TARGETS, T25_MSTAT_ROUTE, T25_TARGET_BY_CODE, T25_ROUTE_BY_CODE };
@@ -82,6 +83,7 @@ export const T25_ATOMIC_CARDS = [
   ...T25_ATOMIC_V4_113_116,
   ...T25_ATOMIC_V4_117_118,
   ...T25_ATOMIC_V4_119_122,
+  ...T25_ATOMIC_V4_123_126,
 ].sort((a, b) => a.routeOrder - b.routeOrder);
 
 if (T25_MSTAT_TARGETS.length !== 80) throw new Error(`T25 audited target manifest must contain 80 targets; found ${T25_MSTAT_TARGETS.length}.`);
