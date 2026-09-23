c1446767d0f9e6a793b1731057c72a9d594f0fe4
Karuma-jojo <kareemgambit@gmail.com>
2026-09-23 21:20:40 +0530

    Add SMMC ledger aggregator

---
+++ course/smmc/ledger.mjs
@@ -0,0 +1,7 @@
+import y2017 from "./ledger-2017.mjs";
+import y2018 from "./ledger-2018.mjs";
+
+export const SMMC_LEDGER_YEARS = [2017, 2018];
+export const SMMC_PROBLEMS = [...y2017, ...y2018];
+
+export default SMMC_PROBLEMS;
