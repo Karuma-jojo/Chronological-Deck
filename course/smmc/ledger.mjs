f5457e9d6a0113c2c1cdf02ef54fce4b63499049
Karuma-jojo <kareemgambit@gmail.com>
2026-09-23 22:01:17 +0530

    Extend SMMC ledger through 2020

--- course/smmc/ledger.mjs
+++ course/smmc/ledger.mjs
@@ -1,8 +1,9 @@
 import y2017 from "./ledger-2017.mjs";
 import y2018 from "./ledger-2018.mjs";
 import y2019 from "./ledger-2019.mjs";
+import y2020 from "./ledger-2020.mjs";
 
-export const SMMC_LEDGER_YEARS = [2017, 2018, 2019];
-export const SMMC_PROBLEMS = [...y2017, ...y2018, ...y2019];
+export const SMMC_LEDGER_YEARS = [2017, 2018, 2019, 2020];
+export const SMMC_PROBLEMS = [...y2017, ...y2018, ...y2019, ...y2020];
 
 export default SMMC_PROBLEMS;
