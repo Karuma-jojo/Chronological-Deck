f430191a3c8d92e87dd81988373acf3c405ae1d9
Karuma-jojo <kareemgambit@gmail.com>
2026-09-23 22:40:23 +0530

    Complete SMMC ledger through 2025

--- course/smmc/ledger.mjs
+++ course/smmc/ledger.mjs
@@ -5,8 +5,13 @@ import y2020 from "./ledger-2020.mjs";
 import y2021 from "./ledger-2021.mjs";
 import y2022 from "./ledger-2022.mjs";
 import y2023 from "./ledger-2023.mjs";
+import y2024 from "./ledger-2024.mjs";
+import y2025 from "./ledger-2025.mjs";
 
-export const SMMC_LEDGER_YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023];
-export const SMMC_PROBLEMS = [...y2017, ...y2018, ...y2019, ...y2020, ...y2021, ...y2022, ...y2023];
+export const SMMC_LEDGER_YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
+export const SMMC_PROBLEMS = [
+  ...y2017, ...y2018, ...y2019, ...y2020, ...y2021,
+  ...y2022, ...y2023, ...y2024, ...y2025,
+];
 
 export default SMMC_PROBLEMS;
